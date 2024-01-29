f6b166e472101a98f380801f146fadc0b02e838c
RIG360 <office@rimpfl.at>
2024-01-29 17:43:47 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-keller-1",
+      "name": "Keller (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.6328250131220958,
+        "pitch": -0.026114348119133268,
+        "fov": 1.6820569460893378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6228861173297426,
+          "pitch": 0.48318620943712887,
+          "rotation": 0,
+          "target": "1-keller-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-keller-2",
+      "name": "Keller (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.6245808683282572,
+        "pitch": -0.0061743660036874815,
+        "fov": 1.6820569460893378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.594982878391253,
+          "pitch": 0.560647673999302,
+          "rotation": 0,
+          "target": "0-keller-1"
+        },
+        {
+          "yaw": -3.1260050200537783,
+          "pitch": 0.38441240847031466,
+          "rotation": 0,
+          "target": "2-keller-3"
+        },
+        {
+          "yaw": -3.127462719106978,
+          "pitch": 0.20866637578137315,
+          "rotation": 0,
+          "target": "3-keller-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-keller-3",
+      "name": "Keller (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.6417239115944344,
+        "pitch": -0.01971160411395445,
+        "fov": 1.6820569460893378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.141037439089807,
+          "pitch": 0.434641752509318,
+          "rotation": 0,
+          "target": "3-keller-4"
+        },
+        {
+          "yaw": 0.041538550861620394,
+          "pitch": 0.33461092397473635,
+          "rotation": 0,
+          "target": "1-keller-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-keller-4",
+      "name": "Keller (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.5634768633235367,
+        "pitch": -0.007977485832903142,
+        "fov": 1.6820569460893378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05977606531126156,
+          "pitch": 0.36738606088658976,
+          "rotation": 0,
+          "target": "2-keller-3"
+        },
+        {
+          "yaw": 0.04148815296869479,
+          "pitch": 0.20105813887223434,
+          "rotation": 0,
+          "target": "1-keller-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Staud40, Keller_240129",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
